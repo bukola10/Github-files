7abb3b6de8f43e2d158836762a5ee55deaaf035d
bukola10 <oibuoye@gmail.com>
2018-05-12 13:05:04 -0400

    mnv
    gd

---
+++ DATATOOLS.sql
@@ -0,0 +1,36 @@
+
+
+--DATA TOOLS
+-----------Integration, Analysing, and reporting are all created in datatool.
+------------SSDTBI STANDS FOR SQL SERVER DATA TOOLS BUSINESS INTELLIGENCE
+-----------------------Business Intelligence Developer Studio
+------------------------Integration Engeineer
+
+
+-------------------------Data Model 3 types Conceptual model drawings link together(no column)
+--------------------------------2nd one is Logical modeling you go into microsoft word and draw a column under it
+---------------------------------3rd one is physical modeling you put in your data type
+
+
+-----within a solution you can have many project under data tools
+-----SSIS pACKAGES is where the pacakages are Kept
+-------Connection Managers(whatever connetion you create in the manager isavailable for all package
+----------you can either use ODBC OR OLE DB as your source
+
+
+---------to create a new package under data tools right click on SSIS PACKAGES THEN NEW
+----------To bring in an existing package right click and click on ADD EXISTING PARKAGE
+-------------DATA FLOW TASK flow from destination to source or vise versa
+----------------if you dont want to use the sort toolbox you need to right click on the data tool and click advanced editor
+
+-------------if you want to create your own query in data flow you will need to click on Execute SQL Task instead of DAta Flow Task
+-------------------you right click and the click on edit
+
+
+
+-----------merge join is joining all familar columns
+--------------merge is combining two tables and care about sorting
+------------union all does not need sorting but merge two table
+
+
+
